b865878c3258854c5a1b740d6e717becdeab6d4b
Alexey-t <support@uvviewsoft.com>
2018-05-17 14:42:30 +0300

    for last CudaTree

--- __init__.py
+++ __init__.py
@@ -1,5 +1,22 @@
-from .find_headers import get_headers
+import itertools
 
-helper = {
-  'MediaWiki': get_headers,
-}
+def is_line_head(s):
+    if not s.startswith('='):
+        return
+    r = len(list(itertools.takewhile(lambda c: '=' == c, s)))
+    r2 = len(list(itertools.takewhile(lambda c: '=' == c, reversed(s))))
+    if r2 >= r:
+        return r
+
+
+def get_headers(filename, lines):
+    '''
+    Generates headers in format:
+    line_index, header_level, header_text
+    '''
+    for i, s in enumerate(lines):
+        if not s.strip():
+            continue
+        r = is_line_head(s)
+        if r:
+            yield i, r, s.strip(' =')
